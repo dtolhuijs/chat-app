fb042eeb54c8e35122ad3cbcb7e8b3dcf548e50b
dtolhuijs <deisi.bonilla@gmail.com>
2016-07-12 15:01:28 +0200

    add messaging

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
+
 import React from 'react';
+import Utils from './lib/Utils';
 import Header from './containers/header';
 import Channel from './containers/channel';
 import MessageInput from './containers/message-input';
+import SetUsername from './containers/set-username';
 
 class App extends React.Component {
-  constructor () {
-      super();
+  constructor() {
+    super();
 
-      this.state = {
-        messages: [],
-        currentUser: "guest"
-      };
+    this.utils = new Utils();
+    let currentUser = this.utils.store("chat.currentUser");
+
+    if (currentUser.length === 0) { currentUser = "guest"; }
+    console.log(currentUser);
+
+    this.state = {
+      messages: [],
+      currentUser: currentUser
+    };
   }
 
-    render() {
-        return (
-          <div>
-            <Header appName="SLAKO"/>
-            <Channel messages={ this.state.messages } />
-            <MessageInput username={ this.state.currentUser } />
-          </div>
-        );
-    }
+  setUsername(newUsername) {
+    this.utils.store("chat.currentUser", newUsername);
+
+    this.setState({
+      currentUser: newUsername
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header appName="SLAKO" />
+        <SetUsername
+          username={ this.state.currentUser }
+          onChange={ this.setUsername.bind(this) } />
+
+        <Channel messages={ this.state.messages } />
+        <MessageInput username={ this.state.currentUser } />
+      </div>
+    );
+  }
 }
 
 export default App;
